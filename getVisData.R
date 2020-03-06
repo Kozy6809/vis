52f8c6d5148c151a069ec6252488cc5c7b700366
wakui <wakui@kcn.jp>
2020-03-06 15:52:50 +0900

    コーンプレート互換性の検証

--- getVisData.R
+++ getVisData.R
@@ -3,6 +3,6 @@ getVisData <- function() {
 library("RODBC")
 ch<-odbcConnect("qc")
 format(Sys.Date()-3652, "#%Y/%m/%d#")->day
-t<-sqlQuery(ch, 
+sqlQuery(ch, 
             paste("select input_date, seizou_code, lot_no, day_10, day_100, week_10, week_100, heat_10, heat_100 from 粘度データ where input_date > ", day, "and judgement = 0 order by seizou_code, input_date;"))
 }
